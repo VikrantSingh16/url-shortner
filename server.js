864bdb09b1b8301935fa835526f1eb3ccab324fe
vikrant-simplemail <vikrant@simplemail.ai>
2024-05-09 13:56:55 +0530

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const UrlRouter = require('./routes/url')
 const UserRouter = require('./routes/user')
 const UrlRedirectRouter = require('./routes/urlroute')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+app.use(cors())
 app.set('view engine','ejs')
 
 
@@ -13,5 +15,5 @@ app.use('/shorturls',UrlRouter)
 app.use('/user',UserRouter)
 app.use('/',UrlRedirectRouter)
 const PORT = process.env.PORT || 3000
-app.listen((PORT),(req,res)=>{console.log('Server is running at 3000');})
+app.listen((PORT),(req,res)=>{console.log('Server is running at '+PORT);})
 
